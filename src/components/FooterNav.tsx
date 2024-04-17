49d40c3d312fad014ba635cd761329e370afad55
James LeVoir <james.levoir@go.minneapolis.edu>
2024-04-17 02:40:06 -0500

    Knew I forgot something. Fixed FooterNav src.

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -4,9 +4,9 @@ const FooterNav = () => {
   return (
     <nav className="container" id="footer-nav">
         <ul>
-            <li><a href="https://www.linkedin.com/in/jameslevoir/" target="blank"><img src="/public/SVG/linkedin.svg" /></a></li>
-            <li><a href="https://github.com/jameslevoir" target="blank"><img src="/public/SVG/github.svg" /></a></li>
-            <li><a href="mailto:jameslevoir@gmail.com"><img src="/public/SVG/email.svg" /></a></li>
+            <li><a href="https://www.linkedin.com/in/jameslevoir/" target="blank"><img src="/SVG/linkedin.svg" /></a></li>
+            <li><a href="https://github.com/jameslevoir" target="blank"><img src="/SVG/github.svg" /></a></li>
+            <li><a href="mailto:jameslevoir@gmail.com"><img src="/SVG/email.svg" /></a></li>
         </ul>
     </nav>
   )
